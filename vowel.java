62fef350e0be853de973d49414b2b8f532895d98
RaghinRadhakrishnan <30748414+RaghinRadhakrishnan@users.noreply.github.com>
2018-01-08 16:54:36 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+impoet java.io.*;
+public class JavaProgram
+{
+public static void main(String args[])
+{
+char ch;
+Scanner scan = new Scanner(System.in);
+System.out.print("Enter an Alphabet : ");
+ch = scan.next().charAt(0);
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+{
+System.out.print("Vowel");
+}
+else
+{
+System.out.print("consonant");
+}
+}
+}
