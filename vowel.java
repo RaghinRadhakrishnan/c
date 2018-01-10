93a19e3ecf76d347e6707360fb8fcdc5f26f4a41
RaghinRadhakrishnan <30748414+RaghinRadhakrishnan@users.noreply.github.com>
2018-01-10 15:46:40 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 impoet java.io.*;
-public class JavaProgram
+public class vowel
 {
 public static void main(String args[])
 {
 char ch;
 Scanner scan = new Scanner(System.in);
 System.out.print("Enter an Alphabet : ");
-ch = scan.next().charAt(0);
+ch=scan.next().charAt(0);
 if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
 {
-System.out.print("Vowel");
+System.out.println("Vowel");
 }
 else
 {
-System.out.print("consonant");
+System.out.println("consonant");
 }
 }
 }
